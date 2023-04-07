8712db76441a07fc2c5e872a94ab34975ec2688b
Aleksandr Tsven <86127025+Ikiso@users.noreply.github.com>
2023-04-07 17:13:10 +0300

    Update bid.go

--- pkg/database/bid.go
+++ pkg/database/bid.go
@@ -6,9 +6,7 @@ func (s *Server) GetAllBids() ([]models.Bid, []models.Bid, error) {
 	var bids []models.Bid
 	var nonDuplicateBids []models.Bid
 	err := s.Db["AUCTION"].Table("bid").Find(&bids).Error
-	//err = s.Db["AUCTION"].Raw("SELECT DISTINCT bid.company_id, bid.creator_id, bid.email, bid.name, bid.phone, bid.rating FROM bid").
-	//	Find(&nonDuplicateBids).Error
-
+	
 	err = s.Db["AUCTION"].Raw("SELECT DISTINCT bid.company_id, bid.name FROM bid GROUP BY bid.company_id, bid.name ORDER BY company_id").
 		Find(&nonDuplicateBids).Error
 
